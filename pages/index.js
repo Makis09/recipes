15eeb23ef2936e8b510e66dbff6d0fdd11802ae0
MarjanCrnichin09 <marjan.crnichin@gmail.com>
2020-12-04 18:36:37 +0100

    material-ui & scss

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
 import Head from "next/head";
 import Link from "next/Link";
-import styles from "../styles/Home.module.css";
+import css from "../styles/test.scss";
 import { getAllRecipes } from "../lib/recipes";
+import Button from "@material-ui/core/Button";
+import { Grid } from "@material-ui/core";
 
 export default function Home(props) {
   const allRecipes = getAllRecipes();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         <h1>bezveze</h1>
-        {allRecipes.map((recipe) => (
-          <Link
-            href={`/${
-              recipe.name.toLowerCase().replace(/\s/g, "-") + "&&" + recipe.id
-            }`}
-            key={recipe.name}
-          >
-            <a style={{ margin: "20px" }}>{recipe.name}</a>
-          </Link>
-        ))}
+        <Grid container spacing={1}>
+          {allRecipes.map((recipe) => (
+            <Grid container key={recipe.name} item xs={4}>
+              <Link
+                href={`/${
+                  recipe.name.toLowerCase().replace(/\s/g, "-") +
+                  "&&" +
+                  recipe.id
+                }`}
+              >
+                <a className={css.test}>{recipe.name}</a>
+              </Link>
+              <Button variant="contained">Default</Button>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </div>
   );
